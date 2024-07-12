095e9faf85116a52677a1f3a45f552fa70007e2d
Rabby <mdrabbybd4747@gmail.com>
2024-07-13 02:53:44 +0600

    customize single product page

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -2,6 +2,7 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import baseApi from "@/redux/api/baseApi"
 import { Star } from "lucide-react"
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa"
@@ -68,6 +69,69 @@ const SingleProduct = () => {
 							</div>
 						</div>
 					</div>
+				</div>
+				<div>
+					<Tabs
+						defaultValue="description"
+						className="flex flex-col items-center"
+					>
+						<TabsList className="w-full">
+							<TabsTrigger
+								value="description"
+								className="h-full flex-1 text-lg"
+							>
+								Description
+							</TabsTrigger>
+							<TabsTrigger value="review" className="h-full flex-1 text-lg">
+								Product Review
+							</TabsTrigger>
+							<TabsTrigger value="shipping" className="h-full flex-1 text-lg">
+								Shipping and Delivery
+							</TabsTrigger>
+						</TabsList>
+						<TabsContent value="description">
+							{product?.description}
+						</TabsContent>
+						<TabsContent value="review">
+							Review content would be here.
+						</TabsContent>
+						<TabsContent value="shipping">
+							<div className="grid grid-cols-4 gap-5">
+								<div className="col-span-1">
+									<img
+										src="https://woodmart.b-cdn.net/handmade/wp-content/uploads/sites/3/2017/06/wood-ship-1.jpg"
+										alt=""
+									/>
+								</div>
+								<div className="col-span-1">
+									<img
+										src="https://woodmart.b-cdn.net/handmade/wp-content/uploads/sites/3/2017/06/wood-ship-3.jpg"
+										alt=""
+									/>
+								</div>
+								<div className="col-span-2">
+									<strong>1. Processing Time</strong>
+									<p className="mb-3">
+										All orders are processed within 1-2 business days. Orders
+										are not shipped or delivered on weekends or holidays. If we
+										are experiencing a high volume of orders, shipments may be
+										delayed by a few days. Please allow additional days in
+										transit for delivery. If there will be a significant delay
+										in shipment of your order, we will contact you via email or
+										telephone.
+									</p>
+									<strong>2. Shipping Rates & Delivery Estimates</strong>
+									<p>
+										Shipping charges for your order will be calculated and
+										displayed at checkout. We offer the following shipping
+										options: Standard Shipping: 5-7 business days Expedited
+										Shipping: 2-3 business days Overnight Shipping: 1-2 business
+										days Delivery delays can occasionally occur.
+									</p>
+								</div>
+							</div>
+						</TabsContent>
+					</Tabs>
 				</div>
 			</div>
 		</div>
